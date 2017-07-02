92a5b2214d515a54f2254c54eb1f1e3e4b338b4b
Alexjdv <ajdriver19@gmail.com>
2017-07-02 21:50:17 +0100

    added / and /about routes, added redux, react-router and react-router redux, still need to debug the push functions in the navbar

---
+++ src/containers/about/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Content from '../../components/content';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class About extends React.Component {
+
+    render() {
+        return (
+          <div>
+            <Content text="about page content" />
+          </div>
+        );
+    }
+}
+
+export default About;
---
+++ src/containers/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Content from '../../components/content';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Home extends React.Component {
+
+    render() {
+        return (
+          <div>
+            <Content text="home page content" />
+          </div>
+        );
+    }
+}
+
+export default Home;
